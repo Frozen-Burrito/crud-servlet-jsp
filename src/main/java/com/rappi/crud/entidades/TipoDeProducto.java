6b043d1e3bf6eb1ef983aa93165b469fbe2b8722
laura_epj4q4k <laura_epj4q4k@LAPTOP-5GA1JTI6>
2022-06-20 09:31:55 -0500

    crear TipoDeProducto

---
+++ src/main/java/com/rappi/crud/entidades/TipoDeProducto.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.rappi.crud.entidades;
+
+/**
+ *
+ * @author laura_epj4q4k
+ */
+public enum TipoDeProducto {
+    DESAYUNO,
+    ENTRADA,
+    SOPA,
+    CARNE,
+    PASTA,
+    SUSHI,
+    TACOS,
+    ENSALADA,
+    BEBIDA,
+    POSTRE 
+}
